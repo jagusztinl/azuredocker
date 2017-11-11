c509215f5c42c70bafa997638bfd223c61693349
Christer Bystrom <zool79@gmail.com>
2017-11-11 15:30:07 +0100

    Header and displaying date

--- collector/static/collector/ui-vue.js
+++ collector/static/collector/ui-vue.js
@@ -27,29 +27,44 @@
 	};
 
     Vue.component('file-item', {
-        props: ['file', 'actions'],
+        props: ['file', 'viewState', 'actions'],
         template: [
-            '<tr>',
-            '<td><input class="filelist_checkbox" type="checkbox" name="{{ file.id }}" data-id="{{ id }}"></input></td>',
-            '<td>{{ file.id }}</td>',
-            '<td>{{ file.name }}</td>',
-            '<td>{{ file.size }}</td>',
-            '<td v-html="htmlStatus"></td>',
-            '<td>',
-                '<span v-if="file.progress">...</span>',
-                '<span v-if="file.jsondata">&nbsp;</span>',
-                '<a style="cursor: hand;" v-if="!file.progress && !file.jsondata" v-on:click="click">Process</a>',
-            '</td>',
+            '<tr v-bind:class="trClass">',
+                '<td><input class="filelist_checkbox" v-on:click="check" type="checkbox" v-bind:checked="viewState.checked"></input></td>',
+                '<td>{{ file.id }}</td>',
+                '<td>{{ file.name }}',
+                    '<div v-if="viewState.expanded">',
+                        '<div class="bg-warning" v-if="file.error"><span style="font-weight: bold">Error: </span>{{ file.error }}</div>',
+                    '</div>',
+                '</td>',
+                '<td>{{ uploadedFromNow }}</td>',
+                '<td>{{ file.size }}</td>',
+                '<td v-html="htmlStatus"></td>',
+                '<td>',
+                    '<span v-if="viewState.progress">...</span>',
+                    '<span v-if="file.jsondata">&nbsp;</span>',
+                    '<a style="cursor: hand;" v-if="!viewState.progress && !file.jsondata" v-on:click="click">Process</a>',
+                '</td>',
             '</tr>'
         ].join(""),
         methods: {
         	click: function() {
         		this.actions.process(this.file.id);
-        	}
+        	},
+            check: function() {
+                this.viewState.checked = !this.viewState.checked;
+            }
         },
         computed: {
+            trClass: function() {
+                if (this.viewState.checked) {
+                    return "info";
+                }
+                return "";
+
+            },
         	htmlStatus: function() {
-                if (this.file.progress) {
+                if (this.viewState.progress) {
                     return HTML_STATUS.PROGRESS;
                 }
         		if (this.file.error) {
@@ -59,7 +74,10 @@
         			return HTML_STATUS.SUCCESS;
         		}
                 return HTML_STATUS.PENDING;
-        	}
+        	},
+            uploadedFromNow: function() {
+                return moment(this.file.created_at).fromNow();
+            }
         }
     });
 
@@ -80,19 +98,52 @@
             },
             tasks: {
                 type: Array
+            },
+            viewState: {
+                allChecked: {
+                    type: Boolean,
+                    default: false
+                }
+            }
+        },
+        methods: {
+            checkAll: function() {
+                var checked = this.viewState.checked = !this.viewState.checked;
+                this.files.forEach(function(item) {
+                    item.viewState.checked = checked;
+
+                });
+
+            }
+        },
+        computed: {
+            allChecked: function() {
+                var i;
+                if (this.files.length === 0) {
+                    return false;
+                }
+                for(i=0;i<this.files.length;i++) {
+                    if (!this.files.viewState.checked) {
+                        return false;
+                    }
+                }
+                return true;
             }
         },
         template: [
-            '<table class="table table-responsive table-striped filelist-container">',
+            '<table class="table table-responsive table-hover table-striped filelist-container">',
             '<thead>',
-            '<th></th>',
+            '<tr>',
+            '<th><input class="filelist_checkbox" v-on:click="checkAll" type="checkbox" v-bind:checked="viewState.allChecked"></th>',
             '<th>Id</th>',
             '<th>Name</th>',
+            '<th>Uploaded</th>',
             '<th>Size</th>',
-            '<th colspan="2">Processed (running: {{tasks.length}})</th>',
+            '<th colspan="2">Processed</th>',
             '</thead>',
+            '</tr>',
             '<tbody>',
-            '<file-item v-for="file in files" v-bind:file="file" v-bind:actions="actions"></file-item>',
+            '<file-item v-for="file in files" v-bind:file="file.file" v-bind:viewState="file.viewState" v-bind:actions="actions"></file-item>',
             '</tbody>',
             '</table>'
         ].join("")
@@ -104,7 +155,7 @@
 
     var app = new Vue({
         el: '#filelist',
-        template: '<file-list v-bind:tasks="tasks" v-bind:files="files" v-bind:actions="actions"></file-list>',
+        template: '<file-list v-bind:tasks="tasks" v-bind:files="files" v-bind:actions="actions" v-bind:viewState="viewState"></file-list>',
         data: {
             files: [],
             tasks: {},
@@ -113,11 +164,13 @@
                     app.process(id);
             	}
             },
-            pollInterval: 2000
+            pollInterval: 2000,
+            viewState: {}
         },
         methods: {
             process: function(id) {
-                this.files.forEach(function(file) {
+                this.files.forEach(function(item) {
+                    var file = item.file, viewState = item.viewState;
                     if (file.id === id) {
                         var args = {};
                         args[file.id] = 'on';
@@ -128,17 +181,15 @@
                             jsonRes.results.forEach(function(task) {
                                 app.tasks[task.task_id] = task;
                             });
-                 //          file.progress = false;
                         }).fail(function(res) {
-                            file.progress = false;
+                            viewState.progress = false;
                             file.error = res;
                         });
-                        file.progress = true;
+                        viewState.progress = true;
                     }
                 });
             },
             pollTasks: function() {
-                console.log("pollTasks enter");
                 var me = this,
                     task_ids = [], key;
                 for(key in this.tasks) {
@@ -156,7 +207,6 @@
                     });
                     task_ids.join("");
                 } else {
-                    console.log("pollTasks no task ids");
                     setTimeout(function() {
                         me.pollTasks();
                     }, this.pollInterval);
@@ -184,16 +234,12 @@
                     res.forEach(function(file) {
                         fileDict[file.id] = file;
                     });
-                    me.files.forEach(function(file) {
-                        newFile = fileDict[file.id];
+                    me.files.forEach(function(item) {
+                        newFile = fileDict[item.file.id];
                         if (newFile) {
-                            ['jsondata', 'error'].forEach(function(key) {
-                                if (newFile.hasOwnProperty(key)) {
-                                    file[key] = newFile[key];
-                                }
-                            });
+                            item.file = newFile;
                         }
-                        file.progress = false;
+                        item.viewState.progress = false;
                     });
                 });
             },
@@ -214,8 +260,14 @@
     	});
     	*/
     	app.files = files.map(function(file) {
-    		file.progress = false;
-    		return file;
+            return {
+                file: file,
+                viewState: {
+                    progress: false,
+                    expanded: file.id == 15,
+                    checked: false
+                }
+            };
     	});
     });
 
