c3d256246d9a84c2747fca7416fc890fc81946a5
Christer Bystrom <zool79@gmail.com>
2017-11-12 08:37:20 +0100

    Added manage command removejson to remove all json objects

---
+++ collector/management/commands/removejson.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from collector.models import JsonData
+
+
+class Command(BaseCommand):
+    help = 'Delete all derived JSON - useful for developing'
+
+#    def add_arguments(self, parser):
+#        parser.add_argument('poll_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        num_deleted = 0
+        num_failed = 0
+        for jd in JsonData.objects.all():
+            try:
+                jd.delete()
+                num_deleted += 1
+            except Exception as e:
+                self.stderr.write(
+                    self.style.WARNING(
+                        "Failed deleting JsonData entry -> {}".format(e)))
+                num_failed += 1
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Deleted {} JsonData objects, {} failed'.format(
+                    num_deleted, num_failed)))
